cd1e7391268f48c73683935b331df906b9e3ea9a
Sebastián <sdelgado@iesrafaelalberti.es>
2024-11-08 10:45:03 +0100

    Terminado

---
+++ app/src/main/java/com/example/ejercicio_eventos_boton/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.ejercicio_eventos_boton
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val mainLayout = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.main)
+        val changeColorButton = findViewById<Button>(R.id.button)
+
+        changeColorButton.setOnClickListener {
+            val color = generateRandomColor()
+            mainLayout.setBackgroundColor(color)
+        }
+    }
+
+    private fun generateRandomColor(): Int {
+        val red = Random.nextInt(256)
+        val green = Random.nextInt(256)
+        val blue = Random.nextInt(256)
+        return Color.rgb(red, green, blue)
+    }
+}
